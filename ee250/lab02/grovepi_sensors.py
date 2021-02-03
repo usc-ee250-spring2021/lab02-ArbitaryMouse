6ee0f4934c401beab33e4143a74f6377ea536002
Ziang Qin <ziangq@usc.edu>
2021-02-02 22:28:13 -0800

    yes

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -45,8 +45,8 @@ if __name__ == '__main__':
         except TypeError:
                 print ("Error")
         if(dis > threshold):
-                setText(threshold.str() + "cm \n " + dis.str() + "cm")
+                setText(str(threshold) + "cm \n " + str(dis) + "cm")
                 setRGB(0,255,0)
         else:
-                setText(threshold.str() + "cm OBJ PRES \n " + dis.str() + "cm")
+                setText(str(threshold) + "cm OBJ PRES \n " + str(dis) + "cm")
                 setRGB(255,0,0)
